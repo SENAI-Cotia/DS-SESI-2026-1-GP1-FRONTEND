5d1c4cf680452cd289d9b75be876bb0e6a345c0c
Kamilly Gomes RIbeiro <kazim.riber@gmail.com>
2026-05-20 08:09:39 -0300

    mudanças

---
+++ js/estoque.js
@@ -0,0 +1,31 @@
+fetch("http://localhost:3000/produtos")
+.then(response => {
+    if(!response.ok){
+        alert("Ocorreu um erro")
+    }
+    return response.json()
+})
+.then(data => {
+    const tabela = document.getElementById("linha-estoque")
+
+    data.forEach(function(produto){
+
+        const linha = document.createElement("tr")
+
+        linha.innerHTML = `
+            <td>${produto.codigo_barra}</td>
+            <td>${produto.nome}</td>
+            <td>${produto.categoria}</td>
+            <td>${produto.estoques[0]?.quantidade}</td>
+            <td>${produto.estoques[0]?.id_filial}</td>
+        `
+
+        tabela.appendChild(linha)
+    })
+})
+
+const produtos = await prisma.produto.findMany({
+    include: {
+        estoque: true
+    }
+})
